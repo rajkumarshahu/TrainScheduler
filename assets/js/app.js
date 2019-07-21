545bb5f2a4874bd05286015c188c39c2a6446aec
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-07-20 23:48:43 -0400

    formatting done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,36 @@
 // Firebase configuration
 let config = {
-    apiKey: "AIzaSyBmHotrGNs2RQF1UsTfuNMpmRkPAxzlaxc",
-    authDomain: "database-a8975.firebaseapp.com",
-    databaseURL: "https://database-a8975.firebaseio.com",
-    projectId: "database-a8975",
-    storageBucket: "",
-    messagingSenderId: "190994548371",
-    appId: "1:190994548371:web:83d3fd7afea41bf9"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(config);
-  let database = firebase.database();
+  apiKey: "AIzaSyBmHotrGNs2RQF1UsTfuNMpmRkPAxzlaxc",
+  authDomain: "database-a8975.firebaseapp.com",
+  databaseURL: "https://database-a8975.firebaseio.com",
+  projectId: "database-a8975",
+  storageBucket: "",
+  messagingSenderId: "190994548371",
+  appId: "1:190994548371:web:83d3fd7afea41bf9",
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+let database = firebase.database();
 
-  let trainName = "";
-  let destination = "";
-  let firstTime = "";
-  let tFrequency = 0;
+let trainName = "";
+let destination = "";
+let firstTime = "";
+let tFrequency = 0;
 
 function currentTime() {
-  let current = moment().format('LT');
+  let current = moment().format("LT");
   $(".current-time").html(`${current}`);
   setTimeout(currentTime, 1000);
-};
+}
 
 $("#submit-btn").on("click", e => {
-  let timeStr = $("#first-train-input").val().trim();
-  let validTimeStr = (timeStr.search(/^\d{2}:\d{2}$/) != -1) &&
-                      (timeStr.substr(0,2) >= 0 && timeStr.substr(0,2) < 24) &&
-                      (timeStr.substr(3,2) >= 0 && timeStr.substr(3,2) <= 59);
+  let timeStr = $("#first-train-input")
+    .val()
+    .trim();
+  let validTimeStr =
+    timeStr.search(/^\d{2}:\d{2}$/) != -1 &&
+    (timeStr.substr(0, 2) >= 0 && timeStr.substr(0, 2) < 24) &&
+    (timeStr.substr(3, 2) >= 0 && timeStr.substr(3, 2) <= 59);
 
   $(".alert-danger").hide();
 
@@ -42,11 +45,15 @@ $("#submit-btn").on("click", e => {
     $("#frequency-input")
       .val()
       .trim() === "" ||
-      $("#first-train-input").val().trim() === ""
+    $("#first-train-input")
+      .val()
+      .trim() === ""
   ) {
-    $(".alert-div").html(`<div class="alert-danger text-center">Some of your inputs are missing!!!</div>`);
+    $(".alert-div").html(
+      `<div class="alert-danger text-center">Some of your inputs are missing!!!</div>`
+    );
     return false;
-  } else if(validTimeStr) {
+  } else if (validTimeStr) {
     name = $("#name-input")
       .val()
       .trim();
@@ -69,52 +76,61 @@ $("#submit-btn").on("click", e => {
       frequency: frequency,
       dateAdded: firebase.database.ServerValue.TIMESTAMP,
     });
-    $(".alert-div").html(`<div class="alert-success text-center">${name} Schedule added successfully!!!</div>`);
+    $(".alert-div").html(
+      `<div class="alert-success text-center">${name} Schedule added successfully!!!</div>`
+    );
     return true;
-  }else{
-    $(".alert-div").html(`<div class="alert-danger text-center">Invalid First Train Time format!!!</div>`);
+  } else {
+    $(".alert-div").html(
+      `<div class="alert-danger text-center">Invalid First Train Time format!!!</div>`
+    );
     return false;
   }
 });
 
 $(document).on("click", ".remove", function() {
-  if(confirm(`Are you sure you want to delete this?`)){
-    $(".alert-div").html(`<div class="alert-info text-center"> Schedule Deleted!!!</div>`);
+  if (confirm(`Are you sure you want to delete this?`)) {
+    $(".alert-div").html(
+      `<div class="alert-info text-center"> Schedule Deleted!!!</div>`
+    );
     keyref = $(this).attr("data-key");
-    database.ref().child(keyref).remove();
+    database
+      .ref()
+      .child(keyref)
+      .remove();
     mainfunction();
   }
-
 });
 
-let mainfunction = function () {
+let mainfunction = function() {
   $("tbody").html("");
-  database.ref().on("child_added", (snapshot)=> {
+  database.ref().on("child_added", snapshot => {
     populateTrainSchedule(snapshot);
-    });
-}
+  });
+};
 
-let populateTrainSchedule = function (snapshot) {
+let populateTrainSchedule = function(snapshot) {
   let newPost = snapshot.val();
-    trainName = newPost.name;
-    destination = newPost.destination;
-    firstTime = newPost.firstTrainTime;
-    tFrequency = newPost.frequency;
-    let firstTimeConverted = moment(firstTime, "LT").subtract(1, "years");
-    let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    let tRemainder = diffTime % tFrequency;
-    let tMinutesTillTrain = tFrequency - tRemainder;
-    let nextTrain = moment().add(tMinutesTillTrain, "minutes").format("LT");
+  trainName = newPost.name;
+  destination = newPost.destination;
+  firstTime = newPost.firstTrainTime;
+  tFrequency = newPost.frequency;
+  let firstTimeConverted = moment(firstTime, "LT").subtract(1, "years");
+  let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  let tRemainder = diffTime % tFrequency;
+  let tMinutesTillTrain = tFrequency - tRemainder;
+  let nextTrain = moment()
+    .add(tMinutesTillTrain, "minutes")
+    .format("LT");
 
-    let newRecord =$(`<tr class="arrival"><td>${trainName}</td>
+  let newRecord = $(`<tr class="arrival"><td>${trainName}</td>
                         <td>${destination}</td>
                         <td class="text-center">${tFrequency}</td>
                         <td class="text-center">${nextTrain}</td>
                         <td class="text-center">${tMinutesTillTrain}</td>
                         <td class="text-center"><button class="remove fa fa-trash-o btn-danger bordered rounded m-1" data-key = ${snapshot.key}></button></td></tr>`);
-    $(".data-container").append(newRecord);
-
-}
+  $(".data-container").append(newRecord);
+};
 
 currentTime();
 
